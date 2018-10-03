d51098a4f772876d9a4287872ceb0505cb10266e
Denesvargas <37148037+Denesvargas@users.noreply.github.com>
2018-10-03 19:19:20 -0300

    Add files via upload

--- TrabalhoSO/buffer.c
+++ TrabalhoSO/buffer.c
@@ -6,7 +6,8 @@ Buffer* buffer_inicializa(int cap){
     Buffer* buff = (Buffer*) malloc(sizeof(Buffer*));
     buff->buffer = (char*) malloc(cap);
     buff->cap = cap;
-    buff->index = 0;
+    buff->index_ins = 0;
+    buff->index_rem = 0;
     return buff;
 }
 
@@ -17,22 +18,35 @@ void buffer_finaliza(Buffer *buf){
 }
 
 bool buffer_insere(Buffer *buf, void *p, int tam){
-    printf("cuzao\n");
     if(tam <= 0)
         return 0;
     char* aux = (char*) p;
-    int i, j;
-    for(j = 0; buf->index < buf->cap && j <= tam; j++){
-        buf->buffer[buf->index++] = aux[j];
+    int j;
+    for(j = 0; buf->index_ins < buf->cap && j < tam; j++){
+        buf->buffer[buf->index_ins++] = aux[j];
     }
-    printf("cuzao");
+    buf->buffer[buf->index_ins++] = 0;
     return 1;
 }
 
 void buffer_imprime(Buffer *buf){
     printf("Conteudo, %d: \n", buf->cap);
-    for(int i = 0; i < buf->index; i++){
+    for(int i = 0; i < buf->index_ins; i++){
         printf("%c ",buf->buffer[i]);
     }
     printf("\n");
+}
+
+bool buffer_remove(Buffer *buf, void *p, int cap, int *tam){
+    if(tam <= 0)
+        return 0;
+    char *aux = (char*) p;
+    int i;
+    for(i = 0; buf->buffer[buf->index_rem] != 0; i++){
+        if(i < cap)
+            aux[i] = buf->buffer[buf->index_rem++];
+        else
+            buf->index_rem++;
+    }
+    *tam = i;
 }
--- TrabalhoSO/main.c
+++ TrabalhoSO/main.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "buffer.h"
 
 int main(){
     Buffer* buf = buffer_inicializa(10000);
     char* teste1 = "abcdef", *teste2 = "bbbbbbbb";
     buffer_insere(buf, teste1, strlen(teste1));
-    printf("cuzaomain\n");
     buffer_insere(buf, teste2, strlen(teste2));
     buffer_imprime(buf);
+    int tam = 0, i;
+    void *p = (void*) malloc(6);
+    int r = buffer_remove(buf, p, 6, &tam);
+    printf("%d removidos", tam);
+    char *aux = (char*) p;
+    for(i = 0; i < 7; i++){
+        printf("%c", aux[i]);
+    }
+    printf("\n");
+    int r = buffer_remove(buf, p, 6, &tam);
+    printf("%d removidos", tam);
+    char *aux = (char*) p;
+    for(i = 0; i < 7; i++){
+        printf("%c", aux[i]);
+    }
+    printf("\n");
 }
