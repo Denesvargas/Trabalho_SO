0ddbf80ccfcaa5e9e2e0c76ed34ce5184a5e0f1c
Denesvargas <37148037+Denesvargas@users.noreply.github.com>
2018-10-16 17:53:51 -0300

    Add files via upload

--- TrabalhoSO1/main.c
+++ TrabalhoSO1/main.c
@@ -1,42 +1,100 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include "buffer.h"
 
-void adiciona(){
+#define nthread_add 1330
+#define nthread_rem 1100
 
-}
+pthread_mutex_t count_mutex;
 
-void remove(){
+void* adiciona_thr(void *p);
+void* remove_thr(void *p);
 
-}
+typedef struct{
+    Buffer *buf;
+    pthread_mutex_t *mutex;
+    char *aux;
+}thread_arg_add;
+
+typedef struct{
+    Buffer *buf;
+    pthread_mutex_t *mutex;
+    int size;
+}thread_arg_rem;
 
 int main(){
     Buffer* buf = buffer_inicializa(100);
-    char *teste = "nnnnnnnnnn", *teste2 = "fedcbaa";
-    void *p1 = (void*) teste;
-    void *p2 = (void*) teste2;
-    int s = buffer_insere(buf, p1, strlen(teste));
-    buffer_imprime(buf);
-    printf("\n");
-    s = buffer_insere(buf, p2, strlen(teste2));
-    buffer_imprime(buf);
-    int tam = 0, i, r;
-    void *p = (void*) malloc(6);
-    r = buffer_remove(buf, p, 6, &tam);
-    char *caralho = (char*) p;
-    printf("%d removidos\n", tam);
-    for(i = 0; i < 6; i++){
-        printf("-%c", caralho[i]);
+    int i;
+    char *teste[] = {"aaaaaaaaaaa","bbbbbbbbbbbbbb","cccccccccccccc","ddddddddddddd"};
+    pthread_t t_add[nthread_add], t_rem[nthread_rem];
+    thread_arg_add args_add[nthread_add];
+    thread_arg_rem args_rem[nthread_rem];
+    for(i = 0; i < nthread_add; i++){
+        args_add[i].aux = teste[i % 4];
+        args_add[i].buf = buf;
+        args_add[i].mutex = &count_mutex;
     }
-    printf("-\n");
-    r = buffer_remove(buf, p, 7, &tam);
-    if(r == 1){
-        printf("%d removidos\n", tam);
-        char *aux2 = (char*) p;
-        for(i = 0; i < 6; i++){
-            printf("-%c", aux2[i]);
-        }
+    for(i = 0; i < nthread_rem; i++){
+        args_rem[i].size = 10;
+        args_rem[i].buf = buf;
+        args_rem[i].mutex = &count_mutex;
+    }
+    pthread_mutex_init(&count_mutex, NULL);
+    for(i = 0; i < nthread_add; i++){
+        pthread_create(&(t_add[i]), NULL, adiciona_thr, &(args_add[i]));
+    }
+    for(i = 0; i < nthread_rem; i++){
+        pthread_create(&(t_rem[i]), NULL, remove_thr, &(args_rem[i]));
+    }
+    for(i = 0; i < nthread_add; i++){
+        int r = pthread_join(t_add[i], NULL);
+         r = pthread_join(t_rem[i], NULL);
+    }
+    printf("FIM\n");
+    buffer_imprime(buf);
+}
+
+void* adiciona_thr(void *p){
+    thread_arg_add *p_arg = (thread_arg_add*) p;
+    Buffer *buf = p_arg->buf;
+    char *aux = p_arg->aux;
+    pthread_mutex_t *mutex_temp = p_arg->mutex;
+    void *p_aux = (void*) aux;
+    int tam = strlen(aux);
+    pthread_mutex_lock(mutex_temp);
+    int r = buffer_insere(buf, p_aux, tam);
+    if(!r)
+        printf("Nao adicionou no buffer\n");
+    else{
+        buffer_imprime(buf);
         printf("\n");
     }
+    pthread_mutex_unlock(mutex_temp);
+    return 0;
+}
+
+void* remove_thr(void *p){
+    thread_arg_rem *p_arg = (thread_arg_rem*) p;
+    Buffer *buf = p_arg->buf;
+    int siz_rem =p_arg->size;
+    int tam = 0, r = 0, i;
+    pthread_mutex_t *mutex_temp = p_arg->mutex;
+    void *p_aux = (void*) malloc(siz_rem);
+    pthread_mutex_lock(mutex_temp);
+    r = buffer_remove(buf, p_aux, siz_rem, &tam);
+    if(r){
+        char *caralho = (char*) p_aux;
+        printf("%d removidos\n", tam);
+        if(siz_rem > tam)
+            siz_rem = tam;
+        for(i = 0; i < siz_rem; i++){
+            printf("-%c", caralho[i]);
+        }
+        printf(" -> %d foram pedidos\n", i);
+    }
+    else
+        printf("Nao removeu");
+    pthread_mutex_unlock(mutex_temp);
 }
